9e117cbf9202c9021f113cb791592669408fc99a
Mykhailo Karpenko <me@mkarp.co>
2018-04-09 10:47:46 +0200

    Recent changes

--- .bash_profile
+++ .bash_profile
@@ -6,6 +6,5 @@ source "$DOT/ps1.bash"
 source "$DOT/shell_aliases.bash"
 source "$DOT/shell_dev.bash"
 
-export PATH="/usr/local/bin:${PATH}"
 export CLICOLOR=1
 export LSCOLORS=ExFxBxDxCxegedabagacad
--- shell_dev.bash
+++ shell_dev.bash
@@ -1,3 +1,7 @@
 export BOOT_JVM_OPTIONS='-Xmx2g -client -XX:+TieredCompilation -XX:TieredStopAtLevel=1 -Xverify:none --add-modules java.xml.bind'
 
-eval "$(rbenv init -)"
+export PATH="$(pyenv root)/shims:$(rbenv root)/shims:/usr/local/bin:${PATH}"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
